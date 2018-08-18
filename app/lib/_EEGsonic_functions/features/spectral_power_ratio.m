82cbd0fc768b02361e8542396f225aaf2513aea0
Bl0101 <yacine.mahdid@live.com>
2018-08-17 22:53:44 -0400

    ADD functions structures

--- app/lib/_EEGsonic_functions/features/spectral_power_ratio.m
+++ app/lib/_EEGsonic_functions/features/spectral_power_ratio.m
@@ -1,5 +1,5 @@
-function [outputArg1,outputArg2] = spectrogram(inputArg1,inputArg2)
-%SPECTROGRAM Summary of this function goes here
+function [outputArg1,outputArg2] = spectral_power_ratio(inputArg1,inputArg2)
+%SPECTRAL POWER RATIO Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
---
+++ app/lib/_EEGsonic_functions/launch_sonification.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = launch_sonification(inputArg1,inputArg2)
+%LAUNCH_SONIFICATION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
