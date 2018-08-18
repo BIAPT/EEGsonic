ca76802c5a5246be339f269f730685a5e005e85d
Bl0101 <yacine.mahdid@live.com>
2018-08-17 22:47:33 -0400

    ADD dummy function structures

---
+++ app/lib/_EEGsonic_functions/features/hub_location.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = hub_location(inputArg1,inputArg2)
+%HUB_LOCATION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ app/lib/_EEGsonic_functions/features/topographic_distribution.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = topographic_distribution(inputArg1,inputArg2)
+%TOPOGRAPHIC_DISTRIBUTION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
