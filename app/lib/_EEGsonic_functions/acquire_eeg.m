c38ba8d121cf2c4e8fe82716d93c980a37708d3a
Bl0101 <yacine.mahdid@live.com>
2018-08-24 18:53:44 -0400

    ADD unique folder name creation

--- app/lib/_EEGsonic_functions/acquire_eeg.m
+++ app/lib/_EEGsonic_functions/acquire_eeg.m
@@ -2,10 +2,14 @@ function acquire_eeg(information,parameters)
 %ACQUIRE_EEG Summary of this function goes here
 %   Detailed explanation goes here
     index = 0;
+    folder_name = information.folder_name + "-" + string(now);
+    data_directory = information.saving_directory +"\"+folder_name; 
+    mkdir(data_directory);
     while(1)
         % here depending on the eeg system we will acquire eeg in diff ways
-        data = rand(128,10000);
-        parsave(information.saving_directory,index,data);
+        data = rand(128,10000);% TODO change this to the data acquisition
+        %of whatever system we selected to use
+        parsave(data_directory,index,data);
         index = index + 1;
     end
 end
