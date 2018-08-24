cf92767abd700ebfd94b0abcb5a064cbe1ac3f44
Bl0101 <yacine.mahdid@live.com>
2018-08-24 18:31:47 -0400

    ADD Parallele saving features

--- app/lib/_EEGsonic_functions/io/parsave.m
+++ app/lib/_EEGsonic_functions/io/parsave.m
@@ -1,6 +1,6 @@
 function parsave(path,index,data)
 %PARSAVE Wrapper function to save in the paralelle workers
-    file_path = path + index + ".mat";
+    file_path = path + "\" + index + ".mat";
     save(file_path,'data');
 end
 
