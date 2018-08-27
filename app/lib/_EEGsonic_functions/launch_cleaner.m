c0a3e8744dd17c712760478809075a4a5be0151a
Bl0101 <yacine.mahdid@live.com>
2018-08-26 23:04:54 -0400

    FIX Bug in functions

--- app/lib/_EEGsonic_functions/launch_cleaner.m
+++ app/lib/_EEGsonic_functions/launch_cleaner.m
@@ -1,17 +1,11 @@
-function launch_cleaner(app)
+function launch_cleaner(parameters)
 %LAUNCH_CLEANER Summary of this function goes here
 %   Detailed explanation goes here
-
-    %% Close the workers
-    cancel(app.information.data_worker);
-    cancel(app.information.features_worker);
-    app.information.data_worker = [];
-    app.information.features_worker = [];
     
     %% Close and clean the amplifiers
-    if(app.parameters.general.egi129.is_selected)
+    if(parameters.general.egi129.is_selected)
         clean_egi129();
-    elseif(app.parameters.general.dsi24.is_selected)
+    elseif(parameters.general.dsi24.is_selected)
         clean_dsi24();
     end
 end
