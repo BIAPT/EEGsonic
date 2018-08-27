01f9a090f1eeb511e5ce0cb2286db4ad5729827f
Bl0101 <yacine.mahdid@live.com>
2018-08-26 22:10:24 -0400

    ADD cleaner backbone when shut off the program

---
+++ app/lib/_EEGsonic_functions/launch_cleaner.m
@@ -0,0 +1,23 @@
+function launch_cleaner(app)
+%LAUNCH_CLEANER Summary of this function goes here
+%   Detailed explanation goes here
+    cancel(app.information.data_worker);
+    cancel(app.information.features_worker);
+    app.information.data_worker = [];
+    app.information.features_worker = [];
+    
+    if(app.parameters.general.egi129.is_selected)
+        clean_egi129();
+    elseif(app.parameters.general.dsi24.is_selected)
+        clean_dsi24();
+    end
+end
+
+function clean_egi129()
+
+end
+
+function clean_dsi24()
+
+end
+
