83253bb0cf8658dda707d79a7c3c6b918509feed
Bl0101 <yacine.mahdid@live.com>
2018-08-28 13:49:41 -0400

    ADD input output pe

--- app/lib/_EEGsonic_functions/features/permutation_entropy.m
+++ app/lib/_EEGsonic_functions/features/permutation_entropy.m
@@ -1,7 +1,7 @@
-function [outputArg1,outputArg2] = permutation_entropy(inputArg1,inputArg2)
+function [avg_pe_frontal,avg_pe_parietal] = permutation_entropy(window_size,frontal_channels,...
+                                            posterior_channels,embedding_dimension,time_delay)
 %PERMUTATION_ENTROPY Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
 end
 
