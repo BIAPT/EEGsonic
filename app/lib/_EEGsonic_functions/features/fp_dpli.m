dfe3b13ae229d271e196a29b11e8252b6c38e65c
Bl0101 <yacine.mahdid@live.com>
2018-08-28 13:45:40 -0400

    ADD input output fp_dpli

--- app/lib/_EEGsonic_functions/features/fp_dpli.m
+++ app/lib/_EEGsonic_functions/features/fp_dpli.m
@@ -1,7 +1,7 @@
-function [outputArg1,outputArg2] = fp_dpli(inputArg1,inputArg2)
+function [avg_dpli_midline,avg_dpli_lateral] = fp_dpli(window_size,midline_channels,lateral_channels,...
+                                               number_surrogates,p_value)
 %FP_DPLI Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
 end
 
