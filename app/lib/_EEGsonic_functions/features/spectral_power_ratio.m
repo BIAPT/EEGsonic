3744130fefcaef85e3aa70bcd1e98a4f213e56d4
Bl0101 <yacine.mahdid@live.com>
2018-08-28 13:35:46 -0400

    ADD input output spr

--- app/lib/_EEGsonic_functions/features/spectral_power_ratio.m
+++ app/lib/_EEGsonic_functions/features/spectral_power_ratio.m
@@ -1,7 +1,10 @@
-function [outputArg1,outputArg2] = spectral_power_ratio(inputArg1,inputArg2)
+function [ratio_beta_alpha,ratio_alpha_theta] = spectral_power_ratio(window_size,time_bandwith_product,number_tapers, ...
+                                                theta_bandwith,is_theta,alpha_bandwith,is_alpha,beta_bandwith,...
+                                                is_beta,averaging_size)
 %SPECTRAL POWER RATIO Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+
+
 end
 
