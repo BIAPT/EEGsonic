745c9f3eca9a5805c3563f3f65aae3cc360ffaea
Bl0101 <yacine.mahdid@live.com>
2018-09-06 13:49:30 -0400

    ADD backbone spr_to_osc

--- app/lib/EEGsonic_functions/osc/conversion/spectral_power_ratio_to_osc.m
+++ app/lib/EEGsonic_functions/osc/conversion/spectral_power_ratio_to_osc.m
@@ -2,5 +2,19 @@ function [ratio_beta_alpha_osc,ratio_alpha_theta_osc] = spectral_power_ratio_to_
 %SPR_TO_OSC Summary of this function goes here
 %   Detailed explanation goes here
 
+    %% TODO:
+    % 1) See Florian spr recommendation on slack for osc string output type
+    % 2) the input are in this format: 1X128, accessible by simple indexing
+    % 3) the string we need to make are to be stored as list
+    
+    ratio_beta_alpha_osc = [];
+    ratio_alpha_theta_osc = [];
+    for i = 1:length(ratio_beta_alpha)
+        
+    end
+    
+    for i = 1:length(ratio_alpha_theta)
+        
+    end
 end
 
