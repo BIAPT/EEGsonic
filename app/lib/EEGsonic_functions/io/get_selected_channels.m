0b4e2e53a0977568464419f87a0902abf7426f7f
Bl0101 <yacine.mahdid@live.com>
2018-09-12 17:20:37 -0400

    ADD get_selected_channels structure

---
+++ app/lib/EEGsonic_functions/io/get_selected_channels.m
@@ -0,0 +1,14 @@
+function [selected_channels] = get_selected_channels(information,parameters)
+%GET_SELECTED_CHANNELS Summary of this function goes here
+%   Detailed explanation goes here
+    
+    %% Seting up Variables
+    selected_channels = struct();
+    
+
+    %% TODO:
+    % create a boolean mask for each analysis technique that has channels
+    % selection and then populate this structure with each of these boolean
+    % mask.
+end
+
