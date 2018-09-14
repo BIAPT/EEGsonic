f587e9b9de5d082b95209b9fc1ba7da734df3d1f
Bl0101 <yacine.mahdid@live.com>
2018-09-14 09:54:41 -0400

    ADD simple td to osc

--- app/lib/EEGsonic_functions/osc/conversion/topographic_distribution_to_osc.m
+++ app/lib/EEGsonic_functions/osc/conversion/topographic_distribution_to_osc.m
@@ -1,7 +1,7 @@
 function [ratio_front_back_osc] = topographic_distribution_to_osc(ratio_front_back)
 %TOPOGRAPHIC_DISTRIBUTION_TO_OSC Summary of this function goes here
 %   Detailed explanation goes here
-
-
+    osc_base_string = "/TopoDistribution/RatioFrontBack ";
+    ratio_front_back_osc = osc_base_string + num2str(ratio_front_back);
 end
 
