8f8071ba2fb7f5888bcfbd37e1e9e6002a738bb2
Bl0101 <yacine.mahdid@live.com>
2018-09-14 09:39:23 -0400

    FIX some bugs in data acquisition

--- app/lib/EEGsonic_functions/main/acquire_eeg.m
+++ app/lib/EEGsonic_functions/main/acquire_eeg.m
@@ -28,10 +28,10 @@ end
 
 function [data] = get_egi_data(parameters)
     %% Setting Variables
-    acquisition_size = parameters.general.egi129.acquisition_size * 1000;
+    acquisition_size = parameters.general.egi129.data_acquisition_size;
     
     %% Random Generation (DEBUGGING ONLY!)
-    data = rand(128,acquisition_size);
+    data = rand(128,acquisition_size*1000);
     pause(acquisition_size);
 end
 
