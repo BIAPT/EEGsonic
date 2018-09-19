872b08f3a1655dcd0ce567dc6acb9a9f3f23d68c
Bl0101 <yacine.mahdid@live.com>
2018-09-18 22:58:11 -0400

    REFACTOR parsave variable name

--- app/lib/EEGsonic_functions/io/parsave.m
+++ app/lib/EEGsonic_functions/io/parsave.m
@@ -1,6 +1,6 @@
-function parsave(path,index,data)
+function parsave(path,name,data)
 %PARSAVE Wrapper function to save in the paralelle workers
-    file_path = path + "\" + index + ".mat";
+    file_path = path + "\" + name + ".mat";
     save(file_path,'data');
 end
 
