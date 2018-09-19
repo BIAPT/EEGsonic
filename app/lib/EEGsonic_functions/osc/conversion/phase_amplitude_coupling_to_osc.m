75c5038ea6e9ce02cb49e96d1b14d743f594d89f
Bl0101 <yacine.mahdid@live.com>
2018-09-19 18:53:05 -0400

    ADD setup for pac

--- app/lib/EEGsonic_functions/osc/conversion/phase_amplitude_coupling_to_osc.m
+++ app/lib/EEGsonic_functions/osc/conversion/phase_amplitude_coupling_to_osc.m
@@ -1,4 +1,4 @@
-function [ratio_peak_through_frontal_osc,ratio_peak_through_parietal_osc] = phase_amplitude_coupling_to_osc(ratio_peak_through_frontal,ratio_peak_through_parietal)
+function [rpt_frontal_osc,rpt_parietal_osc] = phase_amplitude_coupling_to_osc(rpt_frontal,rpt_parietal)
 %PHASE_AMPLITUDE_COUPLING_TO_OSC Summary of this function goes here
 %   Detailed explanation goes here
 
