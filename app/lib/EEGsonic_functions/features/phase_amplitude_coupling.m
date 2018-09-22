e6c80855c154fcccc7437c0e04954e16f7fdf437
Bl0101 <yacine.mahdid@live.com>
2018-09-21 21:16:43 -0400

    ADD phase amplitude couple (2/4) done

--- app/lib/EEGsonic_functions/features/phase_amplitude_coupling.m
+++ app/lib/EEGsonic_functions/features/phase_amplitude_coupling.m
@@ -28,10 +28,8 @@ function [rpt_frontal,rpt_parietal] = phase_amplitude_coupling(eeg_data,eeg_info
     high_frequency_amplitude = abs(hilbert(eeg_high_frequency)); %calculating the amplitude by taking absolute value of hilber
 
 %% Step 2: Calculate the Modulagram
-%{
-    Use the PAC code you have implemented in EEGapp to construct a phase-amplitude modulgram,
-    assigning each temporal sample to one of n = 18 equally spaced phase bins.  
-%}
+    % Use the PAC code you have implemented in EEGapp to construct a phase-amplitude modulgram,
+    % assigning each temporal sample to one of n = 18 equally spaced phase bins.  
 
     %To note: number of bins = 18, as said by slack. May have to make this
     %modular
@@ -40,67 +38,33 @@ function [rpt_frontal,rpt_parietal] = phase_amplitude_coupling(eeg_data,eeg_info
     number_channels = size(eeg_data,1);
     % ! Speedup can be gained over here, leave that for now  ! %
     sorted_amplitude = zeros(number_bins,2);
+    
+    % Here we sort amplitude according to the phase
     for channel = 1:number_channels
         channel_phase = low_frequency_phase(channel,:);
         channel_amplitude = high_frequency_amplitude(channel,:);
-        for i = 1:length(channel_phase)
-            if channel_phase(i) > -pi && channel_phase(i) < -pi + bin_size
-                sorted_amplitude(1,1) = sorted_amplitude(1) + channel_amplitude(i);
-                sorted_amplitude(1,2) = sorted_amplitude(1,2) + 1;
-            elseif channel_phase(i) > -pi + bin_size && channel_phase(i) < -pi + 2*bin_size
-                sorted_amplitude(2,1) = sorted_amplitude(2) + channel_amplitude(i);
-                sorted_amplitude(2,2) = sorted_amplitude(2,2) + 1;
-            elseif channel_phase(i) > -pi + 2*bin_size && channel_phase(i) < -pi + 3*bin_size
-                sorted_amplitude(3) = sorted_amplitude(3) + channel_amplitude(i);
-                sorted_amplitude(3,2) = sorted_amplitude(3,2) + 1;
-            elseif channel_phase(i) > -pi + 3*bin_size && channel_phase(i) < -pi + 4*bin_size
-                sorted_amplitude(4) = sorted_amplitude(4) + channel_amplitude(i);
-                sorted_amplitude(4,2) = sorted_amplitude(4,2) + 1;
-            elseif channel_phase(i) > -pi + 4*bin_size && channel_phase(i) < -pi + 5*bin_size
-                sorted_amplitude(5) = sorted_amplitude(5) + channel_amplitude(i);
-                sorted_amplitude(5,2) = sorted_amplitude(5,2) + 1;
-            elseif channel_phase(i) > -pi + 5*bin_size && channel_phase(i) < -pi + 6*bin_size
-                sorted_amplitude(6) = sorted_amplitude(6) + channel_amplitude(i);
-                sorted_amplitude(6,2) = sorted_amplitude(6,2) + 1;
-            elseif channel_phase(i) > -pi + 6*bin_size && channel_phase(i) < -pi + 7*bin_size
-                sorted_amplitude(7) = sorted_amplitude(7) + channel_amplitude(i);
-                sorted_amplitude(7,2) = sorted_amplitude(7,2) + 1;
-            elseif channel_phase(i) > -pi + 7*bin_size && channel_phase(i) < -pi + 8*bin_size
-                sorted_amplitude(8) = sorted_amplitude(8) + channel_amplitude(i);
-                sorted_amplitude(8,2) = sorted_amplitude(8,2) + 1;
-            elseif channel_phase(i) > -pi + 8*bin_size && channel_phase(i) < -pi + 9*bin_size
-                sorted_amplitude(9) = sorted_amplitude(9) + channel_amplitude(i);
-                sorted_amplitude(9,2) = sorted_amplitude(9,2) + 1;
-            elseif channel_phase(i) > -pi + 9*bin_size && channel_phase(i) < -pi + 10*bin_size
-                sorted_amplitude(10) = sorted_amplitude(10) + channel_amplitude(i);
-                sorted_amplitude(10,2) = sorted_amplitude(10,2) + 1;
-            elseif channel_phase(i) > -pi + 10*bin_size && channel_phase(i) < -pi + 11*bin_size
-                sorted_amplitude(11) = sorted_amplitude(11) + channel_amplitude(i);
-                sorted_amplitude(11,2) = sorted_amplitude(11,2) + 1;
-            elseif channel_phase(i) > -pi + 11*bin_size && channel_phase(i) < -pi + 12*bin_size
-                sorted_amplitude(12) = sorted_amplitude(12) + channel_amplitude(i);
-                sorted_amplitude(12,2) = sorted_amplitude(12,2) + 1;
-            elseif channel_phase(i) > -pi + 12*bin_size && channel_phase(i) < -pi + 13*bin_size
-                sorted_amplitude(13) = sorted_amplitude(13) + channel_amplitude(i);
-                sorted_amplitude(13,2) = sorted_amplitude(13,2) + 1;
-            elseif channel_phase(i) > -pi + 13*bin_size && channel_phase(i) < -pi + 14*bin_size
-                sorted_amplitude(14) = sorted_amplitude(14) + channel_amplitude(i);
-                sorted_amplitude(14,2) = sorted_amplitude(14,2) + 1;
-            elseif channel_phase(i) > -pi + 14*bin_size && channel_phase(i) < -pi + 15*bin_size
-                sorted_amplitude(15) = sorted_amplitude(15) + channel_amplitude(i);
-                sorted_amplitude(15,2) = sorted_amplitude(15,2) + 1;
-            elseif channel_phase(i) > -pi + 15*bin_size && channel_phase(i) < -pi + 16*bin_size
-                sorted_amplitude(16) = sorted_amplitude(16) + channel_amplitude(i);
-                sorted_amplitude(16,2) = sorted_amplitude(16,2) + 1;
-            elseif channel_phase(i) > -pi + 16*bin_size && channel_phase(i) < -pi + 17*bin_size
-                sorted_amplitude(17) = sorted_amplitude(17) + channel_amplitude(i);
-                sorted_amplitude(17,2) = sorted_amplitude(17,2) + 1;
-            elseif channel_phase(i) > -pi + 17*bin_size && channel_phase(i) < -pi + 18*bin_size
-                sorted_amplitude(18) = sorted_amplitude(18) + channel_amplitude(i);
-                sorted_amplitude(18,2) = sorted_amplitude(18,2) + 1;
+        
+        for point_index = 1:length(channel_phase)
+            % Incrementing the amplitude and putting in the right bin
+            for bin = 1:number_bins
+                if(is_phase_in_range(channel_phase,point_index,bin,bin_size))
+                    sorted_amplitude(bin,1) = sorted_amplitude(bin) + channel_amplitude(point_index);
+                    sorted_amplitude(bin,2) = sorted_amplitude(bin,2) + 1;
+                end
             end
         end
     end
+    
+    % Calculate the average amplitude
+    avg_sorted_amplitude = zeros(number_bins,1);
+    for bin = 1:number_bins
+            if sorted_amplitude(bin,2) == 0
+                avg_sorted_amplitude(bin) = 0;
+            else
+                avg_sorted_amplitude(bin) = (sorted_amplitude(bin,1)/sorted_amplitude(bin,2));
+            end
+    end   
+    
 %% Step 3: Find the Through and Peak
 %{
     In order to determine if we have “trough-max” or “peak-max” coupling, for each of the 11 electrodes,
@@ -112,5 +76,9 @@ function [rpt_frontal,rpt_parietal] = phase_amplitude_coupling(eeg_data,eeg_info
 Average the peak / trough ratio for all frontal electrodes, and for all parietal electrodes.
 %}
 
+end
+
+function [is_in_range] = is_phase_in_range(channel_phase,point_index,bin,bin_size)
+    is_in_range = channel_phase(point_index) > -pi + (bin-1)*bin_size && channel_phase(point_index) < -pi + (bin)*bin_size;
 end
 
