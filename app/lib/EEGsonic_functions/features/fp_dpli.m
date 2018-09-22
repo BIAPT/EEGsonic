b04378cc59e948564388f826892b5daa1c943038
Bl0101 <yacine.mahdid@live.com>
2018-09-22 19:17:43 -0400

    ADD dpli first draft and fp_dpli
    still not tested,need to clear up some memory first!

--- app/lib/EEGsonic_functions/features/fp_dpli.m
+++ app/lib/EEGsonic_functions/features/fp_dpli.m
@@ -2,7 +2,13 @@ function [avg_dpli_midline,avg_dpli_lateral] = fp_dpli(eeg_data,eeg_info,paramet
 %FP_DPLI Summary of this function goes here
 %   Detailed explanation goes here
 
-    %% For Steps see fp_wpli
-
+    midline_eeg = eeg_data(midline_mask == 1,:);
+    lateral_eeg = eeg_data(lateral_mask == 1,:);
+    
+    midline_dpli = dpli(midline_eeg,eeg_info,parameters);
+    lateral_dpli = dpli(lateral_eeg,eeg_info,parameters);
+    
+    avg_dpli_midline = mean2(midline_wpli);
+    avg_dpli_lateral = mean2(lateral_wpli);
 end
 
--- app/lib/EEGsonic_functions/features/helper/dpli.m
+++ app/lib/EEGsonic_functions/features/helper/dpli.m
@@ -1,5 +1,21 @@
-function [z_score_dpli] = dpli(eeg_data,eeg_info,parameters)
+function [corrected_dpli] = dpli(eeg_data,eeg_info,parameters)
 %DPLI Summary of this function goes here
 %   Detailed explanation goes here
+
+%% Seting up variables
+%% NOTE: Do we need the p_value? Seems like no from the information in the documentation
+    number_surrogates = parameters.number_surrogates;
+    p_value = parameters.p_value;
+    number_channels = size(eeg_data,1);
+   
+    surrogates_dpli = zeros(number_surrogates,number_channels,number_channels);
+    
+    %% Calculate wPLI
+    uncorrected_dpli = w_PhaseLagIndex(eeg_data); % uncorrected
+    for i = 1:number_surrogates
+        surrogates_dpli(index,:,:) = w_PhaseLagIndex_surrogate(eeg_data);
+    end
+    corrected_dpli = uncorrected_dpli - squeeze(mean(surrogates_dpli,1));
+
 end
 
