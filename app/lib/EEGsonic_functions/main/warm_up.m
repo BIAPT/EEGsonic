122cc796893e27071016d070d75e50938c10394e
Bl0101 <yacine.mahdid@live.com>
2018-09-25 15:06:36 -0400

    ADD warmup skeleton
    Will need to calculate all the features on fake data at least once to speedup the first past of the pipeline

---
+++ app/lib/EEGsonic_functions/main/warm_up.m
@@ -0,0 +1,8 @@
+function warm_up(information,parameters)
+%WARM_UP Summary of this function goes here
+%   Detailed explanation goes here
+    %% Calculate on all analysis techniques some dummy data 
+    % to warm up and avoid the initial overheads
+
+end
+
