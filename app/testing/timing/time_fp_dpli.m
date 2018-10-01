b67a6c35830d2cad7a5e4b6bfc0284e4a87865bd
Bl0101 <yacine.mahdid@live.com>
2018-10-01 15:56:31 -0400

    ADD fp_dpli timing

--- app/testing/timing/time_fp_dpli.m
+++ app/testing/timing/time_fp_dpli.m
@@ -2,9 +2,24 @@ function [time] = time_fp_dpli(information,parameters)
 %TIME_FP_DPLI Summary of this function goes here
 %   Detailed explanation goes here
 
-
-    tic;
+    %% Get variables from information and parameters
+    headset = get_headset(information,parameters);
+    number_channels = headset.number_channels;
+    data_size = parameters.fp_dpli.required_size*1000;
+    boolean_mask = get_selected_channels(information,parameters);
+    channels_location = headset.channels_location;
+    osc = parameters.osc;
     
+    %% Generate the EEG data
+    eeg_data = rand(number_channels,data_size);
+    tic;
+        midline_mask = boolean_mask.fp_dpli.midline_channels;
+        lateral_mask = boolean_mask.fp_dpli.lateral_channels;                     
+        % Calculate fp_dpli
+        [avg_dpli_midline,avg_dpli_lateral] = fp_dpli(eeg_data,headset,parameters.fp_dpli,midline_mask,lateral_mask);                   
+        % Convert and Send to OSC
+        send_fp_dpli(avg_dpli_midline,avg_dpli_lateral,osc);
+        eeg_data = [];
     time = toc;
 end
 
