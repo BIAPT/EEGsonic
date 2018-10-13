836959118a781f2f979b6fa84852b4b28ff44e6b
Bl0101 <yacine.mahdid@live.com>
2018-10-13 17:41:18 -0400

    Add documentation for get_headset

--- app/lib/EEGsonic_functions/io/get_headset.m
+++ app/lib/EEGsonic_functions/io/get_headset.m
@@ -1,6 +1,10 @@
 function [headset] = get_headset(information,parameters)
-%GET_HEADSET Summary of this function goes here
-%   Detailed explanation goes here
+%GET_HEADSET return a headset information
+%   Input:
+%       information: contains static data in the app
+%       parameters: contains variable data as inputted by the user
+%   Output:
+%       headset: the headset informations
     if(parameters.general.egi129.is_selected)
         headset = information.headset.egi129;
     elseif(parameters.general.dsi24.is_selected)
