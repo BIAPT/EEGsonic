bc8c7603399bc1b1017b514bc963c2055c83354a
Bl0101 <yacine.mahdid@live.com>
2018-10-13 19:03:05 -0400

    ADD documention for the hub location

--- app/lib/EEGsonic_functions/features/hub_location.m
+++ app/lib/EEGsonic_functions/features/hub_location.m
@@ -1,19 +1,17 @@
 function [hd_channel_index] = hub_location(eeg_data,eeg_info,parameters)
-%HUB_LOCATION Summary of this function goes here
-%   Detailed explanation goes here
+%HUB_LOCATION choose from the the channels the channel with the highest
+%degree
+%   Input:
+%       eeg_data: data to calculate the measures on
+%       eeg_info: headset information
+%       parameters: variables data as inputed by the user
+%   Output:
+%       hd_channel_index: index of the channel with the highest degree
 
-%% Step 1: Get or calculate wPLI + surrogates
-%{
--	Calculate the wPLI values of all combinations of electrodes,
-    corrected with 10 iterations of surrogate wPLI. 
-%}
+    % Calculate the wPLI for the full eeg
     full_wpli = wpli(eeg_data,eeg_info,parameters);
 
-%% Step 2: Thresholding
-%{
--	Choose the top 10% of wPLI values (this value will also need to be adjustable).
-%}
-
+    % Threshold the wPLI depending on the top connection threshold
     sorted_wpli = sort(full_wpli(:));
     treshold_index = floor((1-parameters.top_connection_threshold)*length(sorted_wpli));
     treshold_value = sorted_wpli(treshold_index);
@@ -22,18 +20,10 @@ function [hd_channel_index] = hub_location(eeg_data,eeg_info,parameters)
     full_wpli(full_wpli >= treshold_value) = 1;
     full_wpli(full_wpli < treshold_value) = 0;
      
-
-    
-%% Step 3: Calculate the Degree of each node
-%{
--	Calculate the degree of each electrode.
-%}
+    %% Caculate the binary degree of the network
     channels_degree = degrees_und(full_wpli);
 
-%% Step 4: Find the location of the electrode
-%{
--	Find the electrode with the highest degree. 
-%}
+    %% Find the channel with the highest degree
     [~,hd_channel_index] = max(channels_degree);
 
 end
