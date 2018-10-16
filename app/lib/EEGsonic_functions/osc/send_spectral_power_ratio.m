f5461efc0d844de9ec7350c228ad3c85b9abc0ee
Bl0101 <yacine.mahdid@live.com>
2018-10-16 16:06:36 -0400

    ADD osc receivers for alpha_theta and beta_alpha (spr)

--- app/lib/EEGsonic_functions/osc/send_spectral_power_ratio.m
+++ app/lib/EEGsonic_functions/osc/send_spectral_power_ratio.m
@@ -14,16 +14,48 @@ function send_spectral_power_ratio(ratio_beta_alpha,ratio_alpha_theta,osc)
     
     %% Create Base string
     for i = 1:number_channels
-        osc_base_string(i) = "/sensor_"+num2str(i)+"/SpecPowerRatio/";
+        osc_base_string(i) = "/sensor_"+num2str(i)+"/spectral_power_ratio/";
     end
     %% Beta/Alpha to OSC
     for i = 1:number_channels
-        ratio_beta_alpha_osc(i) = osc_base_string(i) + "BetaAlpha " + num2str(ratio_beta_alpha(i));
+        ratio_beta_alpha_osc(i) = osc_base_string(i) + "ratio_beta_alpha " + num2str(ratio_beta_alpha(i));
     end
     
     %% Alpha/Theta to OSC
     for i = 1:number_channels
-        ratio_alpha_theta_osc(i) = osc_base_string(i) + "AlphaTheta " + num2str(ratio_alpha_theta(i));
+        ratio_alpha_theta_osc(i) = osc_base_string(i) + "ratio_alpha_theta " + num2str(ratio_alpha_theta(i));
+    end
+    
+    number_osc_receivers = length(osc.path);
+    
+    %% Sending OSC
+    % Send osc data one osc receiver at a time
+    for osc_index = 1:number_osc_receivers
+        % Create a connection to the OSC receivers
+        u = udp(osc.ip{osc_index},str2num(osc.port{osc_index}));
+        %increase the buffer size (input and output)
+        u.InputBufferSize = 3200;
+        u.OutputBufferSize = 3200;
+        
+        % Sending the messages to the OSC receivers
+        fopen(u);
+        
+        %% Sending Ratio Beta Alpha
+        for string_index = 1:length(ratio_beta_alpha_osc)
+            current_osc_message = strsplit(ratio_beta_alpha_osc(string_index)," ");
+            base_osc_message = char(current_osc_message(1));
+            value_osc_message = str2double(current_osc_message(2));
+            oscsend(u,base_osc_message,'f',value_osc_message);    
+        end
+        
+        %% Sending Ratio Alpha Theta
+        for string_index = 1:length(ratio_alpha_theta_osc)
+            current_osc_message = strsplit(ratio_alpha_theta_osc(string_index)," ");
+            base_osc_message = char(current_osc_message(1));
+            value_osc_message = str2double(current_osc_message(2));
+            oscsend(u,base_osc_message,'f',value_osc_message);    
+        end
+        fclose(u);
     end
 end
 
