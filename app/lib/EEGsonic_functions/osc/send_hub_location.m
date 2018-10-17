277a81ac9bd228cd6c3225269a91fdb7c78ab3b7
Bl0101 <yacine.mahdid@live.com>
2018-10-17 10:00:12 -0400

    ADD osc receivers for hub_location + protocol

--- app/lib/EEGsonic_functions/osc/send_hub_location.m
+++ app/lib/EEGsonic_functions/osc/send_hub_location.m
@@ -7,5 +7,33 @@ function send_hub_location(output_type,channel_index,channels_location,osc)
 %       osc: osc data structure
     coordinate = get_coordinate(output_type,channel_index,channels_location);
     
+    %% TODO change below to be coordinate instead of the channel index
+    
+    osc_base_string = "/hub_location/coordinate ";
+    coordinate_osc = osc_base_string + num2str(channel_index); % This last part need to change!
+    
+    %% Sending OSC
+    % Send osc data one osc receiver at a time
+    number_osc_receivers = length(osc.path);
+    for osc_index = 1:number_osc_receivers
+        % Create a connection to the OSC receivers
+        u = udp(osc.ip{osc_index},str2num(osc.port{osc_index}));
+        %increase the buffer size (input and output)
+        u.InputBufferSize = 3200;
+        u.OutputBufferSize = 3200;
+        
+        % Sending the messages to the OSC receivers
+        fopen(u);
+        
+        %% Sending coordinate
+        for string_index = 1:length(coordinate_osc)
+            current_osc_message = strsplit(coordinate_osc(string_index)," ");
+            base_osc_message = char(current_osc_message(1));
+            value_osc_message = str2double(current_osc_message(2));
+            oscsend(u,base_osc_message,'f',value_osc_message); % This also needs to change   
+        end
+        
+        fclose(u);
+    end    
 end
 
