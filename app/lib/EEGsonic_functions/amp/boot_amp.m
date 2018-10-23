3a78e3b3b3fc711785a58e71b77917f34ac61fbc
EGI <egi@EGIs-iMac.local>
2018-10-23 18:47:25 -0400

    edit the boot_amp

--- app/lib/EEGsonic_functions/amp/boot_amp.m
+++ app/lib/EEGsonic_functions/amp/boot_amp.m
@@ -1,10 +1,10 @@
-function boot_amp(amp_type,ip)
+function boot_amp(parameters)
 %BOOT_AMP Summary of this function goes here
 %   Detailed explanation goes here
 
-    if(strcmp(amp_type,"egi129"))
+    if(parameters.egi129.is_selected)
        clean_amp("egi129");
-       ampinit(ip)
+       ampinit('10.10.10.51') %TODO this need to be variable
        ampon;
        ampstart;
     end
