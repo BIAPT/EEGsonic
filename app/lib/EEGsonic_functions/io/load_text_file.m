8be7e409cad7e2db95fc1ccede860e6adc64e0b0
Bl0101 <yacine.mahdid@live.com>
2018-11-02 20:06:57 -0400

    ADD backbone of load_text_file

---
+++ app/lib/EEGsonic_functions/io/load_text_file.m
@@ -0,0 +1,11 @@
+function [text] = load_text_file(file_name)
+%LOAD_TEXT_FILE will read a text file and load its content
+%   Input:
+%       file_name: The name of the file that we need to load the text from
+%   Output:
+%       text: content of the file in string format
+
+
+end
+
+
