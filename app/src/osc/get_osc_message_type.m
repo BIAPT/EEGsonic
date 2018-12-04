ffaa2f55b93f1f98cc45312cf6e8cc12d6b37695
Bl0101 <yacine.mahdid@live.com>
2018-12-04 09:38:01 -0500

    REFACTOR get_osc_message_type to return right amount of f's

--- app/src/osc/get_osc_message_type.m
+++ app/src/osc/get_osc_message_type.m
@@ -1,17 +1,13 @@
-function [type] = get_osc_message_type(data,conversion_type)
+function [type] = get_osc_message_type(data)
 %GET_OSC_MESSAGE_TYPE helper function to output the type of osc message
 %   Input:
 %       data: data that will be sent
-%       conversion_type: type of conversion we want for the data
 %   Output:
 %       type: output type for the message that will become data
-
-    if(strcmp(conversion_type,"matrix"))
         type = "";
         for i = 1:length(data)
             type = strcat(type,"f");
             type = char(type);
         end
-    end
 end
 
