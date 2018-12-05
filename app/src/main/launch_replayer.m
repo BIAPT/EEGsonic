169ea10577c58f88083aa46cf42d43361c4e412e
Bl0101 <yacine.mahdid@live.com>
2018-12-05 10:55:40 -0500

    REFACTOR Launch button behavior

--- app/src/main/launch_replayer.m
+++ app/src/main/launch_replayer.m
@@ -8,7 +8,5 @@ function [features_worker] = launch_replayer(information,parameters)
     index = -1; % Here we start at -1 to pause the feature calculator and make it wait
     save(information.replay_path,'index');
     features_worker = parfeval(p,@calculate_features,0,information,parameters);
-    pause(parameters.warm_up_wait_time); % This is used to give some time for the warmup of the feature calculation
-    
 end
 
