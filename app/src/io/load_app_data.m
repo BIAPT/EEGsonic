ae66c9cb849b2b18a1f05daf984d5042696e2203
Bl0101 <yacine.mahdid@live.com>
2018-12-19 12:30:08 -0500

    ADD state structure in app_data

--- app/src/io/load_app_data.m
+++ app/src/io/load_app_data.m
@@ -1,13 +1,15 @@
-function [information,parameters] = load_app_data(path,is_default)
+function [information,parameters,state] = load_app_data(path,is_default)
 %LOAD_APP_DATA Summary of this function goes here
 %   Detailed explanation goes here
     data = load(path);
     if(is_default)
         information = data.information;
         parameters = data.parameters;
+        state = data.state;
     else
         information = data.app_data.information;
         parameters = data.app_data.parameters;
+        state = data.app_data.state;
     end
 end
 
