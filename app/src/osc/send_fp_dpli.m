e0dc56d837eca61edc805877c876d3a92116e06c
Bl0101 <yacine.mahdid@live.com>
2018-12-19 12:06:56 -0500

    REFACTOR send_fp_dpli
    all the techniques have now a sender that fits the wikis specificiation

--- app/src/osc/send_fp_dpli.m
+++ app/src/osc/send_fp_dpli.m
@@ -8,9 +8,9 @@ function send_fp_dpli(avg_dpli_midline,avg_dpli_lateral,osc)
 %       osc: osc data structure
 
     %% Create the OSC String
-    osc_base_string = "/fp_dpli_midline_and_lateral";
-    avg_dpli_midline_osc = osc_base_string + "avg_dpli_midline " + num2str(avg_dpli_midline);
-    avg_dpli_lateral_osc = osc_base_string + "avg_dpli_lateral " + num2str(avg_dpli_lateral);
+    osc_base = '/fp_dpli_midline_and_lateral';
+    dpli_midline_lateral = [avg_dpli_midline,avg_dpli_lateral];
+    osc_type = get_osc_message_type(dpli_midline_lateral);
     
     %% Sending OSC
     % Send osc data one osc receiver at a time
@@ -25,21 +25,8 @@ function send_fp_dpli(avg_dpli_midline,avg_dpli_lateral,osc)
         % Sending the messages to the OSC receivers
         fopen(u);
         
-        %% Sending avg pli midline
-        for string_index = 1:length(avg_dpli_midline_osc)
-            current_osc_message = strsplit(avg_dpli_midline_osc(string_index)," ");
-            base_osc_message = char(current_osc_message(1));
-            value_osc_message = str2double(current_osc_message(2));
-            oscsend(u,base_osc_message,'f',value_osc_message);    
-        end
-        
-        %% Sending avg pli lateral 
-        for string_index = 1:length(avg_dpli_lateral_osc)
-            current_osc_message = strsplit(avg_dpli_lateral_osc(string_index)," ");
-            base_osc_message = char(current_osc_message(1));
-            value_osc_message = str2double(current_osc_message(2));
-            oscsend(u,base_osc_message,'f',value_osc_message);    
-        end
+        %% Sending dpli midline lateral
+        oscsend(u,osc_base,osc_type,dpli_midline_lateral);    
         
         fclose(u);
     end    
