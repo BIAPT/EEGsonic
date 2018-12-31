4ac46582e57912229c7c7d1137173e71e5b956b1
Bl0101 <yacine.mahdid@live.com>
2018-12-31 11:43:35 -0500

    REFACOTR delay reconstruction

--- app/src/features/helper/delay_reconstruction.m
+++ app/src/features/helper/delay_reconstruction.m
@@ -10,19 +10,19 @@ function output=delay_reconstruction(data, delay_time, embedding_dimension)
 % Heonsoo Lee 2011.11.17
 % Yacine Mahdid 2018.12.31 (Refactored for readability)
 
-%% Initialize variables
-[length,number_channels]=size(data);
-output=zeros(length-delay_time*(embedding_dimension-1),embedding_dimension,number_channels);
+    %% Initialize variables
+    [length,number_channels]=size(data);
+    output=zeros(length-delay_time*(embedding_dimension-1),embedding_dimension,number_channels);
 
-for current_channel=1:number_channels
-    for current_dimension=1:embedding_dimension
-        % Get the start and end times
-        start_time = 1+(current_dimension-1)*delay_time;
-        end_time = length-(embedding_dimension-current_dimension)*delay_time;
-        % slice the data
-        current_data = data(start_time:end_time, current_channel);
-        % store it at the right place
-        output(:, current_dimension, current_channel)= current_data;
+    for current_channel=1:number_channels
+        for current_dimension=1:embedding_dimension
+            % Get the start and end times
+            start_time = 1+(current_dimension-1)*delay_time;
+            end_time = length-(embedding_dimension-current_dimension)*delay_time;
+            % slice the data
+            current_data = data(start_time:end_time, current_channel);
+            % store it at the right place
+            output(:, current_dimension, current_channel)= current_data;
+        end
     end
-end
 
