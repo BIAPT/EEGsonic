4aa04922da91c69afe012b607eaa8ca342456325
Bl0101 <yacine.mahdid@live.com>
2019-01-01 11:24:14 -0500

    REFACTOR convert to get the sampling rate

--- app/src/io/misc/convert.m
+++ app/src/io/misc/convert.m
@@ -5,15 +5,14 @@ function convert(output_path,input_path,input_file,type)
 %   input_file: String of the name of the file we are converting
 %   type: the type of data we are trying to convert
 
-    %% Static variables TODO: make this more dynamic
+    %% Static variables
     app_sampling_rate = 5; % in seconds
-    sampling_frequency = 1000; % (hard coded 1000 Hz)
     
     %% Variables
     data_path = strcat(output_path,filesep,'data');
     
     %% Loading EEG
-    [eeg,~] = load_eeg(input_file,input_path,type);
+    [eeg,sampling_frequency] = load_eeg(input_file,input_path,type);
     recording_length = floor(length(eeg)/sampling_frequency); %in seconds
     max_index = recording_length/5; % Sampling frequency
     
