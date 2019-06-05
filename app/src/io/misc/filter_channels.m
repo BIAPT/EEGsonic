ab293769a08cc8e7f6c975f3208b238c82485966
yacine <yacine.mahdid@mail.mcgill.ca>
2019-06-05 13:31:50 -0400

    fix(bug when changing EEG devices)

---
+++ app/src/io/misc/filter_channels.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = filter_channels(inputArg1,inputArg2)
+%FILTER_CHANNELS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
