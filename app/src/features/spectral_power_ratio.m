068e1bd78bdb897cfc3baac1f7151dffa29ea350
Bl0101 <yacine.mahdid@live.com>
2019-08-29 22:29:31 -0400

    add spr fix at calculate_features level

--- app/src/features/spectral_power_ratio.m
+++ app/src/features/spectral_power_ratio.m
@@ -12,11 +12,8 @@ function [ratio_beta_alpha,ratio_alpha_theta] = spectral_power_ratio(eeg_data,ee
 %NOTE: Gram mistake in the whole app (bandwith -> need to be bandwidth)
     
     %% Setup Variables
+        
     eeg_data = eeg_data';
-    
-    %% Pre processig (only for non-replay)
-    %eeg_data = reref(bpfilter(0.1,50,1000,eeg_data)')';
-    
     %% Create params struct for Chronux function
     params.tapers = [parameters.time_bandwith_product parameters.number_tapers];
     params.Fs = 1000; %eeg_info.sampling_rate;
