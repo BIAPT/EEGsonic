71aac7f1dce0aad71e2cace6fefcaf2d0aa9c20b
Jamie Christopher Webber <jamiecwebber@gmail.com>
2019-10-24 13:20:16 -0400

    attempting to import sound file

--- resonance_gui/udp-browser/web/player.js
+++ resonance_gui/udp-browser/web/player.js
@@ -15,22 +15,29 @@ sound.masterGain = sound.context.createGain();
 sound.masterGain.connect(sound.context.destination);
 
 sound.bufferFiles = [
-	'res1_bass.mp3',
-	'res1_bells.mp3',
-	'res1_cellos.mp3',
-	'res1_clarinet.mp3',
-	'res1_drone.mp3',
-	'res1_flutes.mp3',
-	'res1_guitar.mp3',
-	'res1_violins.mp3']
+	'./samples/res1_bass.mp3',
+	'./samples/res1_bells.mp3',
+	'./samples/res1_cellos.mp3',
+	'./samples/res1_clarinet.mp3',
+	'./samples/res1_drone.mp3',
+	'./samples/res1_flutes.mp3',
+	'./samples/res1_guitar.mp3',
+	'./samples/res1_violins.mp3']
 
 
 fetch(sound.bufferFiles[0], {mode: "cors"})
 	.then(function(resp) {return resp.arrayBuffer()})
 	.then((buffer) => {
 		console.log(buffer);
+		sound.context.decodeAudioData(buffer, (abuffer) => {
+			sound.bufferSources[0] = sound.context.createBufferSource();
+			sound.bufferSources[0].buffer = buffer;
+			sound.bufferSources[0].connect(sound.context.destination);
+			sound.bufferSources[0].loop = true;
+			sound.bufferSources[0].start();
+		});
 	});
 
-sound.bufferSources[0] = sound.context.createBufferSource();
-sound.bufferSources[0]
+
+
 
