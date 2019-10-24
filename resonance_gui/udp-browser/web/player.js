e11b97b86b5953cae0f77fda6507ae728b58b6c4
Jamie Christopher Webber <jamiecwebber@gmail.com>
2019-10-24 13:08:58 -0400

    set up npm html_server to allow CORS requests, in order to access sound files

--- resonance_gui/udp-browser/web/player.js
+++ resonance_gui/udp-browser/web/player.js
@@ -1,1 +1,36 @@
 console.log('player');
+
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+sound = {
+	context : new AudioContext(),
+	masterGain : null,
+	bufferFiles : [],
+	buffers : [],
+	bufferSources : []
+}
+
+sound.masterGain = sound.context.createGain();
+sound.masterGain.connect(sound.context.destination);
+
+sound.bufferFiles = [
+	'res1_bass.mp3',
+	'res1_bells.mp3',
+	'res1_cellos.mp3',
+	'res1_clarinet.mp3',
+	'res1_drone.mp3',
+	'res1_flutes.mp3',
+	'res1_guitar.mp3',
+	'res1_violins.mp3']
+
+
+fetch(sound.bufferFiles[0], {mode: "cors"})
+	.then(function(resp) {return resp.arrayBuffer()})
+	.then((buffer) => {
+		console.log(buffer);
+	});
+
+sound.bufferSources[0] = sound.context.createBufferSource();
+sound.bufferSources[0]
+
